47c2929f71893b508f9efacc9217f858fda3836f
Ma. Mercedes Vergnes <mer.vergnes@gmail.com>
2023-10-02 12:58:44 -0300

    cambios finales

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const club = "UNO Bahía Club";
+var nombre;
 
-alert(`Bienvenid@ a ${club}`);
+function welcome() {
+  alert(`Bienvenid@ a ${club}`);
 
-const nombre = prompt("Por favor, ingrese su Nombre");
-let apellido = prompt("Ingrese su Apellido");
-console.log(nombre + " " + apellido);
+  nombre = prompt("Por favor, ingrese su Nombre");
+  while (!nombre) {
+    alert("*Este campo es requerido");
+    nombre = prompt("Por favor, ingrese su Nombre");
+  }
+  let apellido = prompt("Ingrese su Apellido");
+  while (!apellido) {
+    alert("*Este campo es requerido");
+    apellido = prompt("Por favor, ingrese su Apellido");
+  }
+  console.log(nombre + " " + apellido);
 
-let email = prompt("Ingrese su Email");
-console.log(email);
+  let email = prompt("Ingrese su Email");
+  while (!email) {
+    alert("*Este campo es requerido");
+    email = prompt("Por favor, ingrese su Email");
+  }
+  console.log(email);
 
-alert(`Hola! ${nombre}, nos alegra que hayas decidido incorporarte a ${club}.
+  alert(`Hola! ${nombre}, nos alegra que hayas decidido incorporarte a ${club}.
 Te contamos que actualmente estamos trabajando con un sistema de créditos con el que podés pagar las clases que usas, cuando las usas; y cada actividad tiene un valor en créditos.
 
 A continuación, te mostraremos los Packs que podrás contratar para asistir a las mismas:
@@ -22,80 +36,87 @@ PACK REFUERZO $21.760 - 8 a 40 créditos
 
 PACK ACTITUD $30.720 - 12 a 60 créditos`);
 
-let salir = false;
-let afirmacion = "si";
-let negacion = "no";
-
-while (!salir) {
   let respuestaCorrecta = prompt(`${nombre}, deseas contratar un pack?`);
-
-  if (afirmacion === respuestaCorrecta) {
+  if (respuestaCorrecta == "si") {
     alert("Exelente! Continuemos...");
-    break;
-  } else {
+    selectPack();
+  }
+  else {
     alert(":( Lo sentimos. Esperamos volver a verte pronto.");
-    let salir = true;
+    welcome();
   }
 }
 
-let pack = prompt(`Seleccioná el pack adecuado para vos: 
-1: Pack Recarga
-2: Pack Frecuente
-3: Pack Actitud
-4: Pack Inicial`);
+function selectPack() {
+  let pack = prompt(`Seleccioná el pack adecuado para vos: 
+    1: Pack Recarga
+    2: Pack Frecuente
+    3: Pack Actitud
+    4: Pack Inicial`);
+  var correcto = pack == "1" || pack == "2" || pack == "3" || pack == "4";
 
-switch (pack) {
-  case "1":
-    alert(
-      `Excelente! Haz seleccionado el Pack Recarga. El monto a abonar es de $6.000.-`
-    );
-    break;
-  case "2":
-    alert(
-      `Excelente! Haz seleccionado el Pack Frecuente. El monto a abonar es de $10.000.-`
-    );
-    break;
-  case "3":
-    alert(
-      `Excelente! Haz seleccionado el Pack Actitud. El monto a abonar es de $14.000.-`
-    );
-    break;
-  case "4":
-    alert(
-      `Excelente! Haz seleccionado el Pack Inicial. El monto a abonar es de $3.000.-`
-    );
-    break;
-  default:
+  while (!correcto) {
     alert("La opción ingresada no corresponde a un pack vigente.");
-    break;
-}
+    pack = prompt(`Seleccioná el pack adecuado para vos: 
+    1: Pack Recarga
+    2: Pack Frecuente
+    3: Pack Actitud
+    4: Pack Inicial`);
+    var correcto = pack == "1" || pack == "2" || pack == "3" || pack == "4";
+  }
 
-let mercadoPago = "1";
-let efectivo = "2";
-let cancelar = "0";
-
-let pago = prompt(`${nombre}, cómo deseas abonar?
-1: Mercado Pago 
-2: Efectivo
-0: Cancelar`);
-
-while (pago != "0") {
-  switch (pago) {
+  switch (pack) {
     case "1":
-      alert("Perfecto! te redirigiremos a MP para efectuar el pago. Gracias!");
+      alert(`Excelente! Haz seleccionado el Pack Recarga. El monto a abonar es de $6.000.-`);
+      selectPayment();
       break;
     case "2":
-      alert(
-        "Felicitaciones! Te esperamos en el club para abonar y disfrutar de las clases."
-      );
+      alert(`Excelente! Haz seleccionado el Pack Frecuente. El monto a abonar es de $10.000.-`);
+      selectPayment();
       break;
-    default:
+    case "3":
+      alert(`Excelente! Haz seleccionado el Pack Actitud. El monto a abonar es de $14.000.-`);
+      selectPayment();
+      break;
+    case "4":
+      alert(`Excelente! Haz seleccionado el Pack Inicial. El monto a abonar es de $3.000.-`);
+      selectPayment();
+      break;
+  }
+}
+
+function selectPayment() {
+  let pago = prompt(`${nombre}, cómo deseas abonar?
+  1: Mercado Pago 
+  2: Efectivo
+  0: Cancelar`);
+  var opcionCorrecta = pago == "1" || pago == "2" || pago == "0";
+
+  while (!opcionCorrecta) {
+    alert("La opción ingresada es no es válida.");
+    let pago = prompt(`${nombre}, cómo deseas abonar?
+  1: Mercado Pago 
+  2: Efectivo
+  0: Cancelar`);
+    var opcionCorrecta = pago == "1" || pago == "2" || pago == "0";
+  }
+
+  switch (pago) {
+    case "0":
+      alert("El proceso de pago ha sido cancelado");
       console.log("El proceso de pago ha sido cancelado");
+      welcome();
+      break;
+    case "1":
+      alert("Perfecto! te redirigiremos a MP para efectuar el pago. Gracias!");
+      welcome();
+      break;
+    case "2":
+      alert("Felicitaciones! Te esperamos en el club para abonar y disfrutar de las clases.");
+      welcome();
       break;
   }
 
-  pago = prompt(`${nombre}, cómo deseas abonar?
-   1: Mercado Pago 
-   2: Efectivo
-   0: cancelar`);
 }
+
+welcome();
