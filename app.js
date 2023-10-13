f2953505e84bd677f2d2f39f24fa0b1d7b59b9a9
mmvergnes <mer.vergnes@gmail.com>
2023-10-13 16:43:35 -0300

    implementacion de arrays, objetos, funciones...

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const club = "UNO Bahía Club";
 var nombre;
 
+class Pack {
+  constructor(nombre, creditos, precio) {
+    this.nombre = nombre;
+    this.creditos = creditos;
+    this.precio = precio;
+  }
+}
+
+const pack1 = new Pack("Recarga", 20, "$6.000");
+const pack2 = new Pack("Frecuente", 40, "$10.200");
+const pack3 = new Pack("Actitud", 60, "$14.400");
+const pack4 = new Pack("Inicial", 10, "$3.000");
+
 const metodoDePago = [
   { id: 1, opcion: "Mercado Pago" },
-  { id: 2, opcion: "Efectivo" }
+  { id: 2, opcion: "Efectivo" },
 ];
 
-const opcionCancelar = { id: 0, opcion: "Cancelar" }
-
+const opcionCancelar = { id: 0, opcion: "Cancelar" };
 
 function mensajeBienvenida() {
   alert(`Bienvenid@ a ${club}`);
 
   nombre = prompt("Por favor, ingrese su Nombre");
+
   while (!nombre) {
     alert("*Este campo es requerido");
     nombre = prompt("Por favor, ingrese su Nombre");
@@ -48,8 +61,7 @@ PACK ACTITUD $30.720 - 12 a 60 créditos`);
   if (respuestaCorrecta == "si") {
     alert("Exelente! Continuemos...");
     seleccionarPack();
-  }
-  else {
+  } else {
     alert(":( Lo sentimos. Esperamos volver a verte pronto.");
     mensajeBienvenida();
   }
@@ -75,19 +87,27 @@ function seleccionarPack() {
 
   switch (pack) {
     case "1":
-      alert(`Excelente! Haz seleccionado el Pack Recarga. El monto a abonar es de $6.000.-`);
+      alert(
+        `Excelente! Haz seleccionado el Pack ${pack1.nombre}. El monto a abonar es de ${pack1.precio}.-`
+      );
       seleccionarMetodo();
       break;
     case "2":
-      alert(`Excelente! Haz seleccionado el Pack Frecuente. El monto a abonar es de $10.000.-`);
+      alert(
+        `Excelente! Haz seleccionado el Pack ${pack2.nombre}. El monto a abonar es de ${pack2.precio}.-`
+      );
       seleccionarMetodo();
       break;
     case "3":
-      alert(`Excelente! Haz seleccionado el Pack Actitud. El monto a abonar es de $14.000.-`);
+      alert(
+        `Excelente! Haz seleccionado el Pack ${pack3.nombre}. El monto a abonar es de ${pack3.precio}.-`
+      );
       seleccionarMetodo();
       break;
     case "4":
-      alert(`Excelente! Haz seleccionado el Pack Inicial. El monto a abonar es de $3.000.-`);
+      alert(
+        `Excelente! Haz seleccionado el Pack ${pack4.nombre}. El monto a abonar es de ${pack4.precio}.-`
+      );
       seleccionarMetodo();
       break;
   }
@@ -95,12 +115,15 @@ function seleccionarPack() {
 
 function opcionesDePago() {
   let opcionesDePago = metodoDePago.concat([opcionCancelar]);
-  opcionesDePago = opcionesDePago.reduce((prev, curr) => prev += `    ${curr.id}: ${curr.opcion} \n`, "");
+  opcionesDePago = opcionesDePago.reduce(
+    (prev, curr) => (prev += `    ${curr.id}: ${curr.opcion} \n`),
+    ""
+  );
   return opcionesDePago;
 }
 
 function obtenerPagoPorId(id) {
-  let metodos = metodoDePago.filter(metodo => metodo.id == id)
+  let metodos = metodoDePago.filter((metodo) => metodo.id == id);
   if (metodos.length > 0) {
     return metodos[0];
   }
@@ -108,29 +131,36 @@ function obtenerPagoPorId(id) {
 }
 
 function seleccionarMetodo() {
-
   let opcionCorrecta = false;
   let pagoId;
 
   while (!opcionCorrecta) {
-
     responsePago = prompt(`${nombre}, cómo deseas abonar? 
     \n${opcionesDePago()}`);
 
-    pagoId = parseInt(responsePago)
+    pagoId = parseInt(responsePago);
 
     opcionCorrecta = obtenerPagoPorId(pagoId) || pagoId == 0;
   }
-  console.log(pagoId)
+  console.log(pagoId);
   switch (pagoId) {
-    case "0":
+    case 0:
+      alert("El proceso de pago ha sido cancelado");
       console.log("El proceso de pago ha sido cancelado");
       break;
     case 1:
-      console.log("Perfecto! te redirigiremos a MP para efectuar el pago. Gracias!");
+      console.log(
+        "Perfecto! te redirigiremos a MP para efectuar el pago. Gracias!"
+      );
+      alert("Perfecto! te redirigiremos a MP para efectuar el pago. Gracias!");
       break;
-    case "2":
-      console.log("Felicitaciones! Te esperamos en el club para abonar y disfrutar de las clases.");
+    case 2:
+      console.log(
+        "Felicitaciones! Te esperamos en el club para abonar y disfrutar de las clases."
+      );
+      alert(
+        `Felicitaciones! Te esperamos en el club para abonar tu pack y disfrutar de las clases.`
+      );
       break;
   }
 
